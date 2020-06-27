61ab021f251d9e87154c48aed5516cf4d66a7c63
Matioma <Artiom.Matievschi@gmail.com>
2020-06-27 09:33:59 +0200

    Add destination objective

--- Assets/Scripts/objectives/SpecificQuests/ReachDestination.cs
+++ Assets/Scripts/objectives/SpecificQuests/ReachDestination.cs
@@ -1,38 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class ReachDestination : TutorialObjective
 {
     [SerializeField]
-    GameObject destinationObject;
-    
+    float triggerRange;
+
+    [SerializeField]
+    GameObject destinationObject; //INdicator object
     [SerializeField]
     Vector3 destinationPosition;
 
+    PlayerController player;
+    Transform destination;
 
 
     private void Awake()
     {
-        
+        player =GameObject.FindObjectOfType<PlayerController>();
+
+
+        if (destinationObject != null) {
+            var indicatorObject = Instantiate(destinationObject);
+            destination = indicatorObject.transform;
+            destination.position = destinationPosition;
+        }
     }
 
     protected override void ObjectiveTask()
     {
-        DoneOneObjective();
+        if (destination == null)
+        {
+            Debug.Log("Destination not set");
+
+        }
+
+        Vector3 distanceVector = destination.position - player.transform.position;
+
+        if (distanceVector.sqrMagnitude <= triggerRange)
+        {
+            this.gameObject.SetActive(false);
+            destination.gameObject.SetActive(false);
+            DoneOneObjective();
+        }
     }
 
     private void Update()
     {
+        ObjectiveTask();
     }
 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.transform.GetComponent<PlayerController>() != null) {
-            ObjectiveTask();
-        }
-    }
 
 
 }
