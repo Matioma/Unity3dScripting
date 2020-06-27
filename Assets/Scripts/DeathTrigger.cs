32efc956ac2d957fb6cbdef565deb48da2afbbfe
Matioma <Artiom.Matievschi@gmail.com>
2020-06-27 10:34:30 +0200

    Add Death Trigger

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Stats>()?.DealDamage(int.MaxValue);
+    }
+}
---
+++ Assets/Scripts/objectives/SpecificQuests/KillObjective.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillObjective : TutorialObjective
+{
+
+    Stats target;
+
+
+    private void Awake()
+    {
+        target = FindObjectOfType<EnemyController>().GetComponent<Stats>();
+        //KillObjective killObjective =target.gameObject.AddComponent<KillObjective>();
+        target.OnDeath += ObjectiveTask;
+    }
+
+    protected override void ObjectiveTask()
+    {
+        DoneOneObjective();
+    }
+}
